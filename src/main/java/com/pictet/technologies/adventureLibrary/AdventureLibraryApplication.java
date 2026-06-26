ed5b642deb70f48f85baaa85e9a7d404b465f1e3
rhribeiro25 <renan.shaolin.25@gmail.com>
2026-06-26 14:15:32 +0100

    starting back-end

---
+++ src/main/java/com/pictet/technologies/adventureLibrary/AdventureLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.pictet.technologies.adventureLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureLibraryApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AdventureLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pictet/technologies/adventureLibrary/AdventureLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pictet.technologies.adventureLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
